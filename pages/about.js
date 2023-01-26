24b8f87df8da90dbc1dc5cfad8104019c25c3543
Japheth <japhemchirie@gmail.com>
2023-01-26 16:24:42 +0300

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Meta from "../components/Meta"
+
+const about = () => {
+  return (
+    <div>
+        <Meta title="About" />
+        <h1>Welcome to About Page</h1>
+    </div>
+  )
+}
+
+export default about
